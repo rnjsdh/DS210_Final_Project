537af8f4b73b9d6bf757b8eb733e351fabf03f56
Donghwan Kwon <144952771+rnjsdh@users.noreply.github.com>
2024-05-05 23:07:15 -0400

    Delete proj/target directory
    There was an issue with Github on my Windows, which prevented me from deleting it and even other files. Figured it out after fixing some of the software issues from Windows (described as security issues?).

--- proj/target/release/deps/proj.d
+++
@@ -1,7 +0,0 @@
-C:\Users\dkwon\DS210_Final_Project\DS210_Final_Project\proj\target\release\deps\proj.exe: src\main.rs src\similarity.rs src\file_io.rs
-
-C:\Users\dkwon\DS210_Final_Project\DS210_Final_Project\proj\target\release\deps\proj.d: src\main.rs src\similarity.rs src\file_io.rs
-
-src\main.rs:
-src\similarity.rs:
-src\file_io.rs:
